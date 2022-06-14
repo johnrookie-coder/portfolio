34b46f05e373618979da540363ffab6729dd35fd
John Andrey Galingan <johngalingan05@gmail.com>
2022-06-14 16:04:26 +0800

    Add links to the carousel projects

--- public/main.js
+++ public/main.js
@@ -1,56 +1,56 @@
-"use strict";
-const navigation = document.querySelector(".navigation");
-const mobileMenu = document.querySelector(".mobile__nav");
-const hamburgerMenu = document.querySelector(".hamburger");
-const wrapper = document.querySelector(".wrapper");
+'use strict';
+const navigation = document.querySelector('.navigation');
+const mobileMenu = document.querySelector('.mobile__nav');
+const hamburgerMenu = document.querySelector('.hamburger');
+const wrapper = document.querySelector('.wrapper');
 
-const btnGetStarted = document.querySelector(".btnGetStarted");
-const linksEl = document.querySelector(".navigation__list");
-const backToTop = document.querySelector(".container__top ");
-const headerSection = document.querySelector(".header");
-const skillsSection = document.querySelector(".skills");
-const socialContainer = document.querySelector(".contact__social");
+const btnGetStarted = document.querySelector('.btnGetStarted');
+const linksEl = document.querySelector('.navigation__list');
+const backToTop = document.querySelector('.container__top ');
+const headerSection = document.querySelector('.header');
+const skillsSection = document.querySelector('.skills');
+const socialContainer = document.querySelector('.contact__social');
 
-const carouselList = document.querySelector(".carousel__list");
+const carouselList = document.querySelector('.carousel__list');
 const slides = Array.from(carouselList.children);
-const spanCounter = document.createElement("span");
-const btnNextSlide = document.querySelector(".carousel__btn--right");
-const btnPrevSlide = document.querySelector(".carousel__btn--left");
+const spanCounter = document.createElement('span');
+const btnNextSlide = document.querySelector('.carousel__btn--right');
+const btnPrevSlide = document.querySelector('.carousel__btn--left');
 const slideWidth = slides[0].getBoundingClientRect().width; // To make sure that all img has the same width
-const carouselNav = document.querySelector(".carousel__nav");
+const carouselNav = document.querySelector('.carousel__nav');
 const carouselIndicator = Array.from(
-  document.querySelectorAll(".carousel__indicator")
+  document.querySelectorAll('.carousel__indicator')
 );
 
 let counter = 0;
 
 // Add click event to hamburger menu / mobile version of the website
 const navEvent = function () {
-  navigation.addEventListener("click", (e) => {
+  navigation.addEventListener('click', (e) => {
     if (
-      e.target.closest(".navigation__icon") ||
-      e.target.classList.contains("navigation__icon")
+      e.target.closest('.navigation__icon') ||
+      e.target.classList.contains('navigation__icon')
     ) {
-      mobileMenu.classList.toggle("is-active");
-      hamburgerMenu.classList.toggle("is-active");
+      mobileMenu.classList.toggle('is-active');
+      hamburgerMenu.classList.toggle('is-active');
     }
   });
 };
 
 // Add "click" event to getStartedButton
 const btnGetStartedEvent = function () {
-  btnGetStarted.addEventListener("click", () => {
+  btnGetStarted.addEventListener('click', () => {
     skillsSection.scrollIntoView();
   });
 };
 
 // Delegate "click" event to each of the links
 const linksEvent = function () {
-  linksEl.addEventListener("click", (e) => {
+  linksEl.addEventListener('click', (e) => {
     e.preventDefault();
 
-    if (e.target.classList.contains("navigation__link")) {
-      const hash = e.target.getAttribute("href");
+    if (e.target.classList.contains('navigation__link')) {
+      const hash = e.target.getAttribute('href');
       const path = document.querySelector(`${hash}`);
 
       path.scrollIntoView();
@@ -58,16 +58,16 @@ const linksEvent = function () {
   });
 };
 
-const linksElMobile = document.querySelector(".navigation__list--mobile");
-linksElMobile.addEventListener("click", (e) => {
+const linksElMobile = document.querySelector('.navigation__list--mobile');
+linksElMobile.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (e.target.classList.contains("navigation__link")) {
-    const hash = e.target.getAttribute("href");
+  if (e.target.classList.contains('navigation__link')) {
+    const hash = e.target.getAttribute('href');
     const path = document.querySelector(`${hash}`);
 
-    hamburgerMenu.classList.remove("is-active");
-    mobileMenu.classList.remove("is-active");
+    hamburgerMenu.classList.remove('is-active');
+    mobileMenu.classList.remove('is-active');
 
     path.scrollIntoView();
   } else return;
@@ -94,14 +94,14 @@ const moveToSlides = function (list, currentSlide, targetSlide) {
   // This will run if the current slide (li element) have nextElement or previousElement.
   if (targetSlide) {
     // Remove the class of "current-slide" to the active slide.
-    currentSlide.classList.remove("current-slide");
+    currentSlide.classList.remove('current-slide');
 
     // The value of the "translateX" is coming from the left property of the next / previous slide.
     // Slide direction: from left to right (negative value given to "left")
     list.style.transform = `translateX(-${targetSlide.style.left})`;
 
     // Add the class of "current-slide" to the next slide.
-    targetSlide.classList.add("current-slide");
+    targetSlide.classList.add('current-slide');
   }
 };
 
@@ -112,7 +112,7 @@ const moveToSlides = function (list, currentSlide, targetSlide) {
 
 const createAndStyleCounter = function (counterEl) {
   // Add the class "carousel__counter"
-  counterEl.classList.add("carousel__counter");
+  counterEl.classList.add('carousel__counter');
   counterEl.textContent = `1/${slides.length}`;
   btnNextSlide.append(counterEl);
 };
@@ -126,21 +126,21 @@ const createAndStyleCounter = function (counterEl) {
 const updateDots = function (currentDot, targetDot) {
   if (targetDot) {
     // Remove the class of "active" of the current dot.
-    currentDot.classList.remove("active");
+    currentDot.classList.remove('active');
 
     // Add the class of "active" to the targetDot.
-    targetDot.classList.add("active");
+    targetDot.classList.add('active');
   }
 };
 
 // Button: NextSlide
-btnNextSlide.addEventListener("click", () => {
+btnNextSlide.addEventListener('click', () => {
   // Get the location of the "current-slide"
-  const currentSlide = carouselList.querySelector(".current-slide");
+  const currentSlide = carouselList.querySelector('.current-slide');
   // Get the location of the "next sibling / li" after the "current-slide"
   const nextSlide = currentSlide.nextElementSibling;
   // Get the location of the active dot
-  const currentDot = carouselNav.querySelector(".active");
+  const currentDot = carouselNav.querySelector('.active');
   // Get the next dot
   const nextDot = currentDot.nextElementSibling;
 
@@ -149,18 +149,18 @@ btnNextSlide.addEventListener("click", () => {
     spanCounter.textContent = `${counter + 1}/${slides.length}`;
     moveToSlides(carouselList, currentSlide, nextSlide);
     updateDots(currentDot, nextDot);
-    btnPrevSlide.classList.remove("carousel__btn--disabled");
+    btnPrevSlide.classList.remove('carousel__btn--disabled');
   }
 
   if (counter === slides.length - 1)
-    btnNextSlide.classList.add("carousel__btn--disabled");
+    btnNextSlide.classList.add('carousel__btn--disabled');
 });
 
 // Button: PrevSlide
-btnPrevSlide.addEventListener("click", () => {
-  const currentSlide = carouselList.querySelector(".current-slide");
+btnPrevSlide.addEventListener('click', () => {
+  const currentSlide = carouselList.querySelector('.current-slide');
   const prevSlide = currentSlide.previousElementSibling;
-  const currentDot = carouselNav.querySelector(".active");
+  const currentDot = carouselNav.querySelector('.active');
   const prevDot = currentDot.previousElementSibling;
 
   if (counter >= 1) {
@@ -168,23 +168,23 @@ btnPrevSlide.addEventListener("click", () => {
     spanCounter.textContent = `${counter + 1}/${slides.length}`;
     moveToSlides(carouselList, currentSlide, prevSlide);
     updateDots(currentDot, prevDot);
-    btnNextSlide.classList.remove("carousel__btn--disabled");
+    btnNextSlide.classList.remove('carousel__btn--disabled');
   }
 
-  if (counter === 0) btnPrevSlide.classList.add("carousel__btn--disabled");
+  if (counter === 0) btnPrevSlide.classList.add('carousel__btn--disabled');
 });
 
 // Add click event for each dots
 carouselIndicator.forEach((indicator, idx) => {
-  indicator.addEventListener("click", (e) => {
+  indicator.addEventListener('click', (e) => {
     // Get the location of the target dot
-    const targetDot = e.target.closest("button");
+    const targetDot = e.target.closest('button');
 
     // Get the location of the "current-slide"
-    const currentSlide = carouselList.querySelector(".current-slide");
+    const currentSlide = carouselList.querySelector('.current-slide');
 
     // Get the location of the "active dot"
-    const currentDot = carouselNav.querySelector(".active");
+    const currentDot = carouselNav.querySelector('.active');
 
     // Get the left property of the "click" dot
     const targetSlide = slides[idx];
@@ -195,23 +195,23 @@ carouselIndicator.forEach((indicator, idx) => {
     updateDots(currentDot, targetDot);
 
     if (counter === 0) {
-      btnPrevSlide.classList.add("carousel__btn--disabled");
-      btnNextSlide.classList.remove("carousel__btn--disabled");
+      btnPrevSlide.classList.add('carousel__btn--disabled');
+      btnNextSlide.classList.remove('carousel__btn--disabled');
       spanCounter.textContent = `${counter + 1}/${slides.length}`;
     } else if (counter === slides.length - 1) {
-      btnPrevSlide.classList.remove("carousel__btn--disabled");
-      btnNextSlide.classList.add("carousel__btn--disabled");
+      btnPrevSlide.classList.remove('carousel__btn--disabled');
+      btnNextSlide.classList.add('carousel__btn--disabled');
       spanCounter.textContent = `${counter + 1}/${slides.length}`;
     } else {
-      btnPrevSlide.classList.remove("carousel__btn--disabled");
-      btnNextSlide.classList.remove("carousel__btn--disabled");
+      btnPrevSlide.classList.remove('carousel__btn--disabled');
+      btnNextSlide.classList.remove('carousel__btn--disabled');
       spanCounter.textContent = `${counter + 1}/${slides.length}`;
     }
   });
 });
 
 // Back to top
-backToTop.addEventListener("click", function () {
+backToTop.addEventListener('click', function () {
   headerSection.scrollIntoView();
 });
 
@@ -220,46 +220,46 @@ const callbackFn = function (entries) {
   const [entry] = entries;
 
   if (!entry.isIntersecting) {
-    wrapper.style.position = "fixed";
-    navigation.style.position = "fixed";
+    wrapper.style.position = 'fixed';
+    navigation.style.position = 'fixed';
 
-    backToTop.classList.add("active");
+    backToTop.classList.add('active');
   }
 
   if (entry.isIntersecting) {
-    wrapper.style.position = "absolute";
-    navigation.style.position = "absolute";
-    backToTop.classList.remove("active");
+    wrapper.style.position = 'absolute';
+    navigation.style.position = 'absolute';
+    backToTop.classList.remove('active');
   }
 };
 
 const options = {
   root: null,
   threshold: 0,
-  rootMargin: "-90px",
+  rootMargin: '-90px',
 };
 
 const headerObserver = new IntersectionObserver(callbackFn, options);
 headerObserver.observe(headerSection);
 
 // Contact Details "click" event
-socialContainer.addEventListener("click", (e) => {
+socialContainer.addEventListener('click', (e) => {
   console.log(e.target);
   if (
-    e.target.classList.contains("contact__social") ||
-    e.target.classList.contains("contact__item")
+    e.target.classList.contains('contact__social') ||
+    e.target.classList.contains('contact__item')
   )
     return;
 
-  if (e.target.classList.contains("contact__social")) {
-    const url = e.target.children[1].children[0].getAttribute("href");
+  if (e.target.classList.contains('contact__social')) {
+    const url = e.target.children[1].children[0].getAttribute('href');
     window.open(url);
   }
 
-  if (e.target.classList.contains("icon__img")) {
+  if (e.target.classList.contains('icon__img')) {
     const designLink = e.target
-      .closest(".contact__item")
-      .children[1].children[0].getAttribute("href");
+      .closest('.contact__item')
+      .children[1].children[0].getAttribute('href');
     window.open(designLink);
   }
 });
